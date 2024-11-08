2e305c4b3be184e9e453d1ae604f50460dc87dba
sgbouldin12 <96607187+sgbouldin12@users.noreply.github.com>
2024-11-07 19:37:55 -0600

    modified:   dist/ApiDocs/Signiant_JobAPI_response.json 	renamed:    dist/assets/browser-C7VhLYlP.js -> dist/assets/browser-Csn6rVkw.js 	deleted:    dist/assets/index-8hz7uFQs.js 	new file:   dist/assets/index-DY-2JeeN.css 	deleted:    dist/assets/index-eEafdrAa.css 	modified:   dist/index.html 	modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,25 @@
+import animate from "tailwindcss-animate"
+
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ["class"],
     content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
-    './app/**/*.{js,jsx}',
-    './src/**/*.{js,jsx}',
-  ],
-  theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
-  },
-  plugins: [require("tailwindcss-animate")],
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+        './pages/**/*.{js,jsx}',
+        './components/**/*.{js,jsx}',
+        './app/**/*.{js,jsx}',
+        './src/**/*.{js,jsx}',
+    ],
+    theme: {
+        extend: {
+            borderRadius: {
+                lg: 'var(--radius)',
+                md: 'calc(var(--radius) - 2px)',
+                sm: 'calc(var(--radius) - 4px)'
+            },
+            colors: {}
+        }
+    },
+    plugins: [animate],
 }
